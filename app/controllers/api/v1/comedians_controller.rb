122e6846c16ff6f174c1bac30b929462e49497f6
Nathan Keller <keller.nathan@gmail.com>
2020-09-04 11:05:29 -0600

    Add sad path controller action if  no comedians are avaiable.

--- app/controllers/api/v1/comedians_controller.rb
+++ app/controllers/api/v1/comedians_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::ComediansController < ApplicationController
   def index
     comedians = Comedian.all
+    return render json: cannot_find_comedians if comedians.empty?
     render json: ComedianSerializer.new(comedians)
   end
+
+  private
+
+    def cannot_find_comedians
+      response.status = 404
+      response.body = 'Sorry, no comedians avaiable.'
+    end
 end
