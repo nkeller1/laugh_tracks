1239de00c3b9a82adbe594ba411713d43306719e
Nathan Keller <keller.nathan@gmail.com>
2020-09-07 10:23:09 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,15 @@ com2 = Comedian.create(
   image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS_6JOIep3YiTWHQ4LyD0OcQoR5XJV7u31tgg&usqp=CAU"
 )
 
+com3 = Comedian.create({
+  name: "Rufio",
+  age: 35,
+  city: "Jamica",
+  image_url: "https://i.ytimg.com/vi/zF1T9-6J4Hg/maxresdefault.jpg"
+  })
+
 com1.tvspecials.create(name: 'All About Me', runtime: 70)
 com1.tvspecials.create(name: 'Not My Special', runtime: 100)
 com2.tvspecials.create(name: 'Comedian2 Special', runtime: 90)
 com2.tvspecials.create(name: 'Really, Kitty?', runtime: 80)
+com3.tvspecials.create(name: 'Yes, I am a Cat', runtime: 200)
