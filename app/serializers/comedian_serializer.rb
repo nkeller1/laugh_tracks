d66be2a1fa3c776c1c46f4dd384a8f8fdff4fd24
Nathan Keller <keller.nathan@gmail.com>
2020-09-07 10:46:53 -0600

    Adjust comedian serializer to include count of tv specials

--- app/serializers/comedian_serializer.rb
+++ app/serializers/comedian_serializer.rb
@@ -1,5 +1,5 @@
 class ComedianSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :age, :city, :image_url
+  attributes :name, :age, :city, :image_url, :count_tv_specials
   has_many :tvspecials
 end
