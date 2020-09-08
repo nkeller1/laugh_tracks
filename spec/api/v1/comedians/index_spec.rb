95ce161a9b71bffe31d9a54b305b20f2672bfffe
Nathan Keller <keller.nathan@gmail.com>
2020-09-08 11:05:17 -0600

    Add edge case for narrowing down search field and proving correct stats

--- spec/api/v1/comedians/index_spec.rb
+++ spec/api/v1/comedians/index_spec.rb
@@ -111,4 +111,26 @@ RSpec.describe "Comeadians Index Page" do
       expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
       expect(response.body).to include("[{\"group_statistics\":[{\"average_age\":33},{\"unique_cities\":[\"Arvada\",\"Denver\"]}]}")
   end
+
+  it "responds with narrowed stats on a git reqquest" do
+      com1 = Comedian.create({
+        name: "Nate",
+        age: 35,
+        city: "Denver",
+        image_url: "https://i.ytimg.com/vi/zF1T9-6J4Hg/maxresdefault.jpg"
+        })
+
+      com2 = Comedian.create(
+        name: "J Dog",
+        age: 30,
+        city: "Arvada",
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS_6JOIep3YiTWHQ4LyD0OcQoR5XJV7u31tgg&usqp=CAU"
+      )
+
+      get '/api/v1/comedians?city=Arvada'
+
+      expect(response).to be_successful
+      expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
+      expect(response.body).to include("[{\"group_statistics\":[{\"average_age\":30},{\"unique_cities\":[\"Arvada\"]}]}")
+  end
 end
