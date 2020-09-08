71d43cc5114a5b8f0d616eb83360c2b806fe5a70
Nathan Keller <keller.nathan@gmail.com>
2020-09-08 11:36:18 -0600

    Create routes and show action in comedian controller

--- app/controllers/api/v1/comedians_controller.rb
+++ app/controllers/api/v1/comedians_controller.rb
@@ -27,11 +27,23 @@ class Api::V1::ComediansController < ApplicationController
     render json: ComedianSerializer.new(comedian, include: [:tvspecials]) if comedian.save
   end
 
+  def show
+    comedian = Comedian.find(params[:id])
+    render json: ComedianSerializer.new(comedian, include: [:tvspecials])
+    rescue ActiveRecord::RecordNotFound
+        render json: cannot_find_comedian
+  end
+
   private
 
     def cannot_find_comedians
       response.status = 404
       response.body = 'Sorry, no comedians avaiable.'
+    end
+
+    def cannot_find_comedian
+      response.status = 404
+      response.body = 'Sorry, this comedian does not exist.'
     end
 
     def missing_attributes
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/comedians', to: 'comedians#index'
+      get '/comedians/:id', to: 'comedians#show'
       post '/comedians', to: 'comedians#create'
     end
   end
